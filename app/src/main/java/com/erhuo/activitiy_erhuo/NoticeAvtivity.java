24bf94965677ba9ad0ddd4e510c3c909d1faf78c
ErHuo <1914840480@qq.com>
2017-12-09 11:17:52 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/erhuo3/SearchActivity.java
    #	app/src/main/res/layout/activity_search.xml

--- app/src/main/java/com/erhuo/activitiy_erhuo/NoticeAvtivity.java
+++ app/src/main/java/com/erhuo/activitiy_erhuo/NoticeAvtivity.java
@@ -1,8 +1,8 @@
 package com.erhuo.activitiy_erhuo;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
