bf79e6d58187498f13f9864c0dfaf48bf48ffd34
ErHuo <1914840480@qq.com>
2017-12-09 22:15:41 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/erhuo3/SearchActivity.java
    #	app/src/main/res/layout/activity_search.xml

--- app/src/main/java/com/erhuo/fragment/RequiringRecordFragment.java
+++ app/src/main/java/com/erhuo/fragment/RequiringRecordFragment.java
@@ -8,9 +8,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.erhuo.activitiy_erhuo.MainActivity;
 import com.erhuo.activitiy_erhuo.MyRecordActivity;
 import com.erhuo.activitiy_erhuo.R;
+import com.erhuo.activitiy_erhuo.SellCommodityEdit;
 import com.erhuo.adapter.RecordAdapter;
 import com.erhuo.entity.Notice;
 
@@ -28,10 +34,14 @@ public class RequiringRecordFragment extends Fragment {
     private View view;
     private List<Notice> tmpList = new ArrayList<>();
 
+    private static final String[] typename={"买入","售出"};
+    private Spinner spinner;
+    private ArrayAdapter<CharSequence> spinnerAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_notice, container, false);
+        view = inflater.inflate(R.layout.fragment_record, container, false);
         MyRecordActivity activity = (MyRecordActivity) getActivity();
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.notice_rev);
         recyclerView.setLayoutManager(new LinearLayoutManager(activity));
@@ -41,7 +51,34 @@ public class RequiringRecordFragment extends Fragment {
             getItem();
         }
         return view;
+
+        spinner = (Spinner) view.findViewById(R.id.TradeRecord_spinner);
+        //将可选内容与ArrayAdapter连接起来，simple_spinner_item是android系统自带样式
+        spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.CommodityType, R.layout.type_spiner_text_item);
+        //设置下拉列表的风格,simple_spinner_dropdown_item是android系统自带的样式，等会自定义修改
+        spinnerAdapter.setDropDownViewResource(R.layout.type_spinner_dropdown_item);
+        //将adapter 添加到spinner中
+        spinner.setAdapter(spinnerAdapter);
+        //添加事件Spinner事件监听
+        spinner.setOnItemSelectedListener(new SpinnerSelectedListener());
+
+
     }
+
+
+    class SpinnerSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                                   long arg3) {
+
+            Toast.makeText(activity.this, (typename[arg2]), Toast.LENGTH_SHORT).show();
+
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+    }
+
 
     private void getItem() {
         //Notice notice = new Notice("asd", "asd", "2017.12.7");
